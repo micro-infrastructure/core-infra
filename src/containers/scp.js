1e36acdf7a4f1d40253cc9b451cae923e8effc52
Reggie <cushingreggie@gmail.com>
2020-03-09 09:52:32 +0100

    update scp

--- src/containers/scp.js
+++ src/containers/scp.js
@@ -9,6 +9,14 @@ function handler(details) {
 		}
 	}
 	const users = encodeBase64(JSON.stringify(user))
+	const env = []
+	env.push({ "name": "NAME", "value": details.name })
+	env.push({ "name": "JWTUSERS", "value": users })
+	if(details.env) {
+		details.env.forEach(e => {
+			env.push(e) 
+		})
+	}
 	let cmd = "sleep 15 && echo $JWTUSERS | base64 -d > /assets/jwtusers.json"
 	cmd += " && echo $INFRA | base64 -d > /assets/infra.json"
 	cmd += " && echo $NETWORK | base64 -d > /assets/network.json"
@@ -23,11 +31,7 @@ function handler(details) {
 						"containerPort": details.containerPort
 					}
 				],
-				"env": [
-					{ "name": "NAME", "value": details.name },
-					{ "name": "JWTUSERS", "value": users }
-
-				],
+				"env": env,
 				"volumeMounts": [
 					{ "name": "ssh-key", "mountPath": "/ssh", "readOnly": true },
 					{ "name": "shared-data", "mountPath": "/shared-data" }
